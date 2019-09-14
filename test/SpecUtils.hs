f730f21afed7b84e5e3b27817f4c54a6679888a5
Armando Ramirez <armando.m.ramirez@gmail.com>
2019-09-14 15:11:03 -0700

    Get SDL.Raw.Rotozoom.hsc linking completely

--- test/SpecUtils.hs
+++ test/SpecUtils.hs
@@ -2,8 +2,18 @@ module SpecUtils (module SpecUtils, module X) where
 
 import Test.Hspec as X
 
+import SDL.Raw as SDL
+import Foreign.C.Types
+import Foreign.Ptr
+import Data.Word
+import Control.Exception (bracket)
+
 p :: String -> IO ()
 p s = putStrLn $ "\x1b[32m" ++ s ++ "\x1b[0m"
 
 (.:) :: a -> b -> (a, b)
 a .: b = (a, b)
+
+withSurface :: Word32 -> CInt -> CInt -> CInt -> Word32 -> Word32 -> Word32 -> Word32 -> (Ptr Surface -> IO a) -> IO a
+withSurface v1 v2 v3 v4 v5 v6 v7 v8 =
+  bracket (createRGBSurface v1 v2 v3 v4 v5 v6 v7 v8) freeSurface
